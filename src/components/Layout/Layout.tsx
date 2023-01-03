cc5bc596ff2662dcc49a2546f77de8bd2f9e28c2
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-03 10:59:08 +0300

    layout structure

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+
+function Layout() {
+  return (
+    <div>Layout</div>
+  )
+}
+
+export default Layout
