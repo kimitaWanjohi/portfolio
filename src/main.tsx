f6f644acaac1dad59d26aa7ad6c5ad82004dbbf2
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-03 09:27:43 +0300

    theming

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import App from './App';
+import './index.css';
+import {theme} from './theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App /> 
+    </ThemeProvider>
   </React.StrictMode>,
 )
