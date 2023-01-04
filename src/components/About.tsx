a3ec9024557781b8a874640b440c0a8af3a41c95
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-04 15:44:45 +0300

    about

---
+++ src/components/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+const About: React.FC = () => {
+    
+    return (
+        <>
+        
+
+        </>
+    )
+}
