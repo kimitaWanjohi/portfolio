61faf6b9a1558a4d47f05a7c02b52d66c3704284
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-04 09:02:22 +0300

    done with footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React from 'react';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <Box sx={{ 
+      bgcolor: 'secondary.dark',
+      color: 'common.white',
+      p: 2,
+      mt: 2,
+      textAlign: 'center',
+      display: 'flex',
+      justifyContent: {xs: 'space-around', md: 'space-between'},
+      flexDirection: {xs: 'column', sm: 'row'},
+      alignItems: 'center',
+      position: 'absolute',
+      bottom: 0,
+      width: '100%',
+      left: 0,
+    }}>
+      <Typography variant="h6" align="center">
+        Designed and Developed by Kimita
+      </Typography>
+      <Typography variant="h6" component='div' align="center">
+        Copyright &copy; {new Date().getFullYear()} {" "}
+        <Typography variant="h6" component='span' color="secondary">
+          KW
+        </Typography>
+      </Typography>
+      <Box sx={{display: 'flex', alignItems: 'center'}}>
+        <IconButton size="small" color="inherit" aria-label="github" href="https://github.com/kimitawanjohi" target="_blank">
+          <GitHubIcon />
+        </IconButton>
+        <IconButton size="small" color="inherit" aria-label="linkedin" href="https://www.linkedin.com/in/kimitawanjohi/" target="_blank">
+          <LinkedInIcon />
+        </IconButton>
+        <IconButton size="small" color="inherit" aria-label="instagram" href="https://www.instagram.com/kimitawanjo/" target="_blank">
+          <InstagramIcon />
+        </IconButton>
+        <IconButton size="small" color="inherit" aria-label="twitter" href="https://twitter.com/kimitaW" target="_blank">
+          <TwitterIcon />
+        </IconButton>
+    </Box>
+  </Box>
   )
 }
 
