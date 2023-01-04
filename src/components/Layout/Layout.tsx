4fb3048c1bc7462cbe368957ad1440a8b4058c3c
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-04 21:56:13 +0300

    page layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,17 +3,26 @@ import Footer from './Footer';
 import Navbar from './Navbar';
 
 import Container from '@mui/material/Container';
+import { styled } from '@mui/material/styles';
+
 
 interface LayoutProps {
     children?: React.ReactNode;
 }
 
+const Main = styled('main')(({ theme }) => ({
+  marginTop: theme.mixins.toolbar.minHeight,
+  paddingBottom: theme.spacing(15),
+}));
+
 const Layout:React.FC<PropsWithChildren<LayoutProps>> = ({children}) =>  {
 
   return ( 
-    <Container maxWidth={false} sx={{minHeight: '100vh', position: "relative" }}>
+    <Container maxWidth={false} sx={{minHeight: '100vh', position: 'relative' }}>
         <Navbar />
-        {children}
+          <Main>
+            {children}
+          </Main>
         <Footer />
     </Container>
   )
