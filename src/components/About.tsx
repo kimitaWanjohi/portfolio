7d3a159c0b132db341e0d3f0060d0fa4f9f27613
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-04 15:56:39 +0300

    about text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,8 +7,17 @@ const About: React.FC = () => {
     
     return (
         <>
-        
-
+            <Box>
+                <Typography variant="h1" component="h2" color="secondary" gutterBottom>
+                    About Me
+                </Typography>
+                <Typography variant="h5" component="div" gutterBottom>
+                I am a Full-stack software engineer with a keen interest in designing elegant solutions to technical problems. <br />
+                I am skilled in problem-solving and can leverage full-stack knowledge and experience to build and scale user-centered software designs. <br />
+                I am a highly motivated self-starter and team player. <br />
+                Also,  I'm a quick learner and have a keen eye for detail. <br />
+                </Typography>
+            </Box>
         </>
     )
 }
