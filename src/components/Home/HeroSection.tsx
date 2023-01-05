051f98672ed2d0aa1880142e2d07fb2049b228fd
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-05 09:25:22 +0300

    intro section

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -3,41 +3,29 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import { useTheme, styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
+import { useTheme } from '@mui/material/styles';
 
 import { TypeAnimation } from 'react-type-animation';
 
-import { useNavigate } from 'react-router-dom';
-
 import HomeMain from '../../assets/home-main.svg';
-
-const RoundPillButton = styled(Button)(({ theme }) => ({
-    borderRadius: '50px',
-    padding: theme.spacing(1, 3),
-    textTransform: 'none',
-    fontWeight: 'bold',
-    fontSize: '1rem',
-    color: theme.palette.secondary.light,
-    backgroundColor: 'transparent',
-    border: `2px solid ${theme.palette.secondary}`,
-}));
 
 
 const HeroSection: React.FC = () => {
 
-    const navigate = useNavigate();
     const theme = useTheme();
     return (
-        <>
+        <Box sx={{
+            height: {xs: 'auto', md: '100vh'},
+            mb: theme.spacing(4)
+        }}>
             <Grid container>
-                <Grid item xs={12} md={7}>
+                <Grid item xs={12} sm={7}>
                     <Box sx={{
                         display: 'flex',
                         flexDirection: 'column',
                         justifyContent: 'center',
                         alignItems: 'flex-start',
-                        height: '450px',
+                        height: '100%'
                     }}>
                         <Typography variant="h3" component="div" sx={{color: 'white'}}>
                             Hi there
@@ -60,30 +48,18 @@ const HeroSection: React.FC = () => {
                         </Typography>
                     </Box>
                 </Grid>
-                <Grid item xs={12} md={5}>
+                <Grid item xs={12} sm={5}>
                     <Box sx={{
                         display: 'flex',
                         justifyContent: 'center',
                         alignItems: 'center',
-                        height: '450px',
+                        height: '100%'
                     }}>
-                        <img src={HomeMain} alt="Home Main" style={{width: '100%', maxHeight: '450px'}} />
-                    </Box>
-                </Grid>
-                <Grid item xs={12}>
-                    <Box sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        background: 'transparent',
-                    }}>
-                        <RoundPillButton variant="outlined" onClick={() => navigate('/about')}>
-                            About Me
-                        </RoundPillButton>
+                        <img src={HomeMain} alt="Home Main" style={{width: '100%'}} />
                     </Box>
                 </Grid>
             </Grid>
-        </>
+        </Box>
     )
 }
 
---
+++ src/components/Home/Introduction.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import { styled, useTheme } from '@mui/material/styles';
+
+import AboutImage from '../../assets/about.png';
+
+
+const Span = styled('span')(({ theme }) => ({
+    color: theme.palette.secondary.main,
+}));
+
+const MbFont =  styled('div')(({ theme }) => ({
+    fontSize: '1.2rem',
+    [theme.breakpoints.down('sm')]: {
+        fontSize: '0.8rem'
+    }
+}));
+
+
+const Introduction: React.FC = () => {
+    const theme = useTheme();
+    return (
+        <>
+            <Box sx={{
+                my: theme.spacing(4),
+                py: theme.spacing(2)
+            }}>
+                <Grid container>
+                    <Grid item xs={12} sm={4}>
+                        <Box sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            height: '100%'
+                        }}>
+                            <img src={AboutImage} alt="About" style={{width: '100%', }} />
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={8}>
+                        <Box sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'flex-start',
+                                height: '100%'
+                            }}
+                        > 
+                            <MbFont>
+                                <Typography variant="h2" component="h2" gutterBottom>
+                                    Let Me <Span>Introduce</Span> Myself
+                                </Typography>
+                                <Typography sx={{ 
+                                    fontSize: 'inherit',
+                                    fontWeight: 'inherit'
+                                }} gutterBottom>
+                                    I am a <Span>Full-stack software engineer</Span> with a keen interest in designing elegant solutions to <Span>technical problems</Span>.
+                                </Typography>
+                                <Typography sx={{ 
+                                    fontSize: 'inherit',
+                                    fontWeight: 'inherit'
+                                }} gutterBottom>
+                                    Skilled in <Span>problem-solving</Span> and can leverage full-stack knowledge and experience to build and scale <Span>user-centered</Span> software designs. <br />
+                                </Typography>
+                                <Typography sx={{ 
+                                    fontSize: 'inherit',
+                                    fontWeight: 'inherit'
+                                }} gutterBottom>
+                                    I am a highly motivated self-starter and team player. <br />
+                                </Typography>
+                                <Typography sx={{ 
+                                    fontSize: 'inherit',
+                                    fontWeight: 'inherit'
+                                }} gutterBottom>
+                                    Also,  I'm a <Span>quick learner</Span> and have a keen eye for detail. <br />
+                                </Typography>
+                            </MbFont>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
+        </> 
+    )
+}
+
+export default Introduction
