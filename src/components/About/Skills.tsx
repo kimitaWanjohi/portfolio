4d349f7f2c147db9fba592ae6290bf34978d34d5
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-05 23:16:41 +0300

    skills done

--- src/components/About/Skills.tsx
+++ src/components/About/Skills.tsx
@@ -2,12 +2,84 @@ import React from 'react';
 
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
 import { styled, useTheme} from '@mui/material/styles';
+
 
 const Span = styled('span')(({ theme }) => ({
     color: theme.palette.secondary.main,
 }));
+
+const skills= [
+    {
+        name: 'React',
+        icon: 'https://img.icons8.com/color/48/000000/react-native.png'
+    },
+    {
+        name: 'Node',
+        icon: 'https://img.icons8.com/color/48/000000/nodejs.png'
+    },
+    {
+        name: 'Express',
+        icon: 'https://img.icons8.com/color/48/000000/express.png'
+    },
+    {
+        name: 'Next',
+        icon: 'https://img.icons8.com/color/2x/nextjs.png'
+    },
+    {
+        name: 'MongoDB',
+        icon: 'https://img.icons8.com/color/48/000000/mongodb.png'
+    },
+    {
+        name: 'Python',
+        icon: 'https://img.icons8.com/color/48/000000/python.png'
+    },
+    {
+        name: 'Django',
+        icon: 'https://img.icons8.com/color/48/000000/django.png'
+    },
+    {
+        name: 'PostgreSQL',
+        icon: 'https://img.icons8.com/color/48/000000/postgreesql.png'
+    },
+    {
+        name: 'Git',
+        icon: 'https://img.icons8.com/color/48/000000/git.png'
+    },
+    {
+        name: 'Docker',
+        icon: 'https://img.icons8.com/color/48/000000/docker.png'
+    },
+    {
+        name: 'Linux',
+        icon: 'https://img.icons8.com/color/48/000000/linux.png'
+    },
+    {
+        name: 'AWS',
+        icon: 'https://img.icons8.com/color/48/000000/amazon-web-services.png'
+    },
+]
+
+const SkillItem = styled(Button)(({ theme }) => ({
+    border: '1.7px solid rgba(200,137,230,.637)!important',
+    borderRadius: '5px',
+    boxShadow: '4px 5px 4px 3px rgba(89,4,168,.137)!important',
+    fontSize: ' 4.5em!important',
+    margin: '15px',
+    opacity: '0.8',
+    padding: '10px!important',
+    overflow: 'hidden',
+    textAlign: 'center',
+    transition: 'all 0.3s ease-in-out',
+    verticalAlign: 'middle',
+    width: '120px',
+    height: '120px',
+}));
+
+
 
 const Skills: React.FC = () => {
     const theme = useTheme();
@@ -17,8 +89,17 @@ const Skills: React.FC = () => {
                 <Typography variant="h4" align="center" color="text.primary" >
                     Professional <Span>Skillset</Span>
                 </Typography>
-                <Stack direction="row" spacing={2} sx={{ width: '100%', justifyContent: 'center', alignItems: 'center' }}>
-                </Stack>
+                <Grid container spacing={3}>
+                    {skills.map((skill, index) => (
+                        <Grid item key={index}>
+                            <Tooltip title={skill.name} placement="top">
+                                <SkillItem>
+                                    <img src={skill.icon} alt={skill.name} />
+                                </SkillItem>
+                            </Tooltip>
+                        </Grid>
+                    ))}
+                </Grid>
             </Box>
         </>
     )
