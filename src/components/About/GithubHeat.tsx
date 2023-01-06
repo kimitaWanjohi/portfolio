d275460f31f6b0c4127b2d5a48a17a3d498f2a00
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-06 09:52:33 +0300

    gheat

--- src/components/About/GithubHeat.tsx
+++ src/components/About/GithubHeat.tsx
@@ -2,23 +2,22 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
+import ReactGithubCalendar from 'react-github-calendar';
 
 const Span = styled('span')(({ theme }) => ({
     color: theme.palette.secondary.main,
 }));
 
 const GithubHeat: React.FC = () => {
+    const [loading, setLoading] = React.useState(true);
   return (
     <>
         <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', mt: 10}}>
             <Typography variant="h4" gutterBottom component="div">
                 Github <Span>Contribution Graph</Span>
             </Typography>
-            <Box sx={{width: '100%', height: 'auto', mt: 5}}>
-                <img src="https://ghchart.rshah.org/1ac6ff/kimitawanjohi" alt="kimita-wanjohi's Github chart" style={{
-                    width: '100%',
-                    height: 'auto'
-                }} />
+            <Box sx={{width: '100%', height: 'auto', mt: 5, display: 'flex', justifyContent: 'center'}}>
+                {<ReactGithubCalendar loading={loading}  username="kimitawanjohi"/>}
             </Box>
         </Box>
     </>
