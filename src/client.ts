41790d4d375caa5ca6148e96d97de5fbe581679f
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-07 17:07:00 +0300

    export client, urlbuilder from client.ts

--- src/client.ts
+++ src/client.ts
@@ -2,10 +2,12 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 
-const client = sanityClient({
+export const client = sanityClient({
     projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
     dataset: import.meta.env.VITE_SANITY_DATASET,
     useCdn: import.meta.env.VITE_SANITY_USE_CDN,
     token: import.meta.env.VITE_SANITY_TOKEN,
     apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
 });
+
+export const builder = imageUrlBuilder(client);
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,40 +4,12 @@ import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
 import ProjectsWrapper from './Projects/ProjectsWrapper';
 import ProjectInterface from './Projects/model';
+import { }
+
 
 const Span = styled('span')(({ theme }) => ({
     color: theme.palette.secondary.main,
 }));
-
-const demoProjects: ProjectInterface[]  = [
-    {
-      title: 'Project 1',
-      description: 'This is a demo project lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
-      image: 'https://source.unsplash.com/random',
-      tags: ['React', 'Next.js', 'Material-UI'],
-      source: 'https://github.com',
-      visit: 'https://google.com',
-      id: 0,
-    },
-    {
-      title: 'Project 2',
-      description: 'This is a demo project lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
-      image: 'https://source.unsplash.com/random',
-      tags: ['React', 'Next.js', 'Material-UI'],
-      source: 'https://github.com',
-      visit: 'https://google.com',
-      id: 1,
-    },
-    {
-      title: 'Project 3',
-      description: 'This is a demo project lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
-      image: 'https://source.unsplash.com/random',
-      tags: ['React', 'Next.js', 'Material-UI'],
-      source: 'https://github.com',
-      visit: 'https://google.com',
-      id: 2,
-    }
-  ];
 
 
 const Projects: React.FC = () => {
