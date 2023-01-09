a5850b7d268965640cec1027b1cef27b124e347f
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-09 20:39:51 +0300

    working on the experience section

---
+++ src/components/About/Experience.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+const TimeLine: React.FC = () => {
+
+    return (
+        <Box
+            sx={{
+                my:10,
+                mx: 8
+            }}
+        >
+            <Typography variant="h4" gutterBottom>
+                Experience
+            </Typography>
+        </Box>
+    )
+}
