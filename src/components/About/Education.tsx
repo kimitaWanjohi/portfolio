8260d9c6eeff905ffd36c636e3489de3b10082d0
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-10 08:10:10 +0300

    Education section

---
+++ src/components/About/Education.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+
+const Span = styled('span')(({theme}) => ({
+    color: theme.palette.secondary.main,
+}))
+
+const Education: React.FC = () => {
+
+    return (
+        <Box sx={{
+            mx: 8,
+            my: 8
+        }}>
+            <Typography variant="h4" gutterBottom>
+                My <Span> Education </Span>Background
+            </Typography>
+            
+        </Box>
+    )
+}
+
+
+export default Education
