a80e9737a9e588f2302fac66637473834b3174cb
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-10 11:40:10 +0300

    About models

--- src/components/About/Education.tsx
+++ src/components/About/Education.tsx
@@ -8,6 +8,24 @@ const Span = styled('span')(({theme}) => ({
     color: theme.palette.secondary.main,
 }))
 
+
+const demoEducation = [
+    {
+        id: 1,
+        school: 'University of California, Irvine',
+        degree: 'B.S. Computer Science',
+        year: '2018 - 2022',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+    {
+        id: 2,
+        school: 'University of California, Irvine',
+        degree: 'B.S. Computer Science',
+        year: '2018 - 2022',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+]
+
 const Education: React.FC = () => {
 
     return (
---
+++ src/components/About/model.tsx
@@ -0,0 +1,15 @@
+export interface EducationInterface {
+    id: number;
+    school: string;
+    degree: string;
+    year: string;
+    description: string;
+}
+
+export interface ExperienceInterface {
+    id: number;
+    company: string;
+    position: string;
+    year: string;
+    description: string;
+}
