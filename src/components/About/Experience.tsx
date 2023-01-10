3a336cb38fbb33bbee3fecf9e351a159569c2205
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-10 07:53:31 +0300

    added experience section in about page

--- src/components/About/Experience.tsx
+++ src/components/About/Experience.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
 
+import Timeline from '../Timeline/Timeline';
 
-const TimeLine: React.FC = () => {
+const Span = styled('span')(({ theme }) => ({
+    color: theme.palette.secondary.main,
+}));
+
+const Experience: React.FC = () => {
 
     return (
         <Box
@@ -12,9 +19,12 @@ const TimeLine: React.FC = () => {
                 mx: 8
             }}
         >
-            <Typography variant="h4" gutterBottom>
-                Experience
+            <Typography variant="h4" align="center" gutterBottom>
+                My Work <Span>Experience</Span>
             </Typography>
+            <Timeline />
         </Box>
-    )
-}
+    );
+};
+
+export default Experience;
