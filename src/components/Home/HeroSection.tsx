0883f2e6917e37cff302d4b56c80f3a13f7cbbbe
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-01-11 21:12:13 +0300

    download resume button

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
-import { useTheme } from '@mui/material/styles';
+import { useTheme, styled } from '@mui/material/styles';
 
 import { TypeAnimation } from 'react-type-animation';
 
 import HomeMain from '../../assets/home-main.svg';
+
+
+const RoundedButton = styled(Button)(({ theme }) => ({
+    borderRadius: '50px',
+    padding: theme.spacing(1, 4),
+    color: theme.palette.text.primary,
+    fontWeight: 400,
+    fontSize: '1.25rem',
+}));
+    
 
 
 const HeroSection: React.FC = () => {
@@ -47,6 +58,16 @@ const HeroSection: React.FC = () => {
                                 ]} 
                             />
                         </Typography>
+                        <div>
+                            <RoundedButton 
+                             variant="outlined" 
+                             href="https://docs.google.com/document/d/1oCrwcj9YvyIFhTeb8A0xTRrhUFxY7yWSXcs5kI8T_BI/export?format=pdf"
+                             color="secondary" 
+                             sx={{mt: theme.spacing(4)}}
+                             >
+                                Download Resume
+                            </RoundedButton>
+                        </div>
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={5}>
